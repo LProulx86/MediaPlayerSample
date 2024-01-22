0fba4adfe1daad64574e9f6a749ac2c5bb1d689e
Laurent Proulx <laurent.proulx@nventive.com>
2024-01-22 09:26:48 -0500

    Add project files.

---
+++ MediaPlayerSample/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace MediaPlayerSample
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
